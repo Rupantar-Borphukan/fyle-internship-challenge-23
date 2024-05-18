da681367bbc5b66a2a98c74482ef248790801582
Rupantar <rupantarborphukan@gmail.com>
2024-05-18 09:00:51 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { SearchComponent } from './search/search.component';
+import { RepoListComponent } from './repo-list/repo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    RepoListComponent
   ],
   imports: [
     BrowserModule,
